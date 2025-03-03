778ed079d32825e4d740d1a341696f3499dc52c1
Suhαni <sadhanaroy198421@gmail.com>
2025-03-03 22:52:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,11 +13,11 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
+API_ID = int(getenv("API_ID", "20457610"))
+API_HASH = getenv("API_HASH", "b7de0dfecd19375d3f84dbedaeb92537")
+BOT_TOKEN = getenv("BOT_TOKEN", "8001382118:AAFnJ_Nrhgq--CK22gzclnUB6nkBtNyimtI")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6833733930").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://jjkbot:jjkbot@botdata.b7rf7.mongodb.net/?retryWrites=true&w=majority&appName=botdata")
 LOG_GROUP = getenv("LOG_GROUP", "")
 CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
